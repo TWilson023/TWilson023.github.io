ebc9ace34ca6e197c695657372e8da3eb9e3d737
Tim Wilson <tim@twilson.net>
2015-08-15 22:14:25 -0400

    1

--- thing/game.js
+++ thing/game.js
@@ -1,3 +1,24 @@
 window.onload = function() {
-	alert("testz");
+
+	var canvas = document.getElementById('gameCanvas');
+	var ctx;
+
+	function init() {
+		ctx = canvas.getContext('2d');
+		window.setInterval(function() {
+			update();
+			render();
+		}, 1000 / 30);
+	}
+
+	function update() {
+
+	}
+
+	function render() {
+		ctx.fillStyle = "#000";
+		ctx.fillRect(0, 0, canvas.width, canvas.height);
+	}
+
+	init();
 };
