c66aab3b114c76573459a6a74541380312ad110a
Tim Wilson <tim@twilson.net>
2015-08-15 22:31:14 -0400

    2

--- thing/game.js
+++ thing/game.js
@@ -21,6 +21,7 @@ window.onload = function() {
 	function draw() {
 		ctx.fillStyle = "#000";
 		ctx.fillRect(0, 0, canvas.width, canvas.height);
+		ctx.translate(canvas.width / 2, canvas.height / 2);
 		player.draw(ctx);
 	}
 
