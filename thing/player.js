e9c3d950dc57a7606d5a4ef5cb20fec0a58c700e
Tim Wilson <tim@twilson.net>
2015-08-15 22:27:39 -0400

    1

--- thing/player.js
+++ thing/player.js
@@ -13,6 +13,7 @@ var Player = function() {
 		var x = Math.cos(angle * (Math.PI / 180)) * r;
 		var y = Math.sin(angle * (Math.PI / 180)) * r;
 
+		ctx.fillStyle = "#FFF";
 		ctx.fillRect(x - radius / 2, y - radius / 2, radius, radius);
 	};
 
